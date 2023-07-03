f995f1d53ea07a63c025e0d032ef3fd25c6f2b4d
Menta <simcictilen.gmail.com>
2023-07-03 10:12:53 +0200

    stampa su schermo pulita

--- index.php
+++ index.php
@@ -25,43 +25,49 @@ class Movie {
     public $duration;
     public $releaseDate;
 
-    public function __construct($title, float $duration, ReleaseDate $releaseDate, string ...$genre ){
+    public function __construct($title, $duration, ReleaseDate $releaseDate, string ...$genre ){
 
         self :: $movieCounter++;
 
         $this -> title = $title;
         $this -> genre = $genre;
-        $this -> duration = number_format($duration, 2);
+        $this -> duration = $duration;
         $this -> releaseDate = $releaseDate;
     }
+    public function getGenreStr(){
+        $res = "";
+        for ($i=0; $i < count ($this -> genre) ; $i++) { 
+            $genre = $this -> genre[$i];
+            $res .=$genre . ", ";
+        }
+        return $res;
+    }
+
 
     public function getMovieDetails(){
 
 
-        return $this -> genre . " " . $this -> duration . " " . $this -> releaseDate;
+        return 
+        $this -> title . "<br>" . 
+        $this -> getGenreStr() . "<br>" . 
+        $this -> duration . "m<br>" . 
+        $this -> releaseDate -> year . "/" . $this -> releaseDate -> month . "/" . $this -> releaseDate -> day;
+    }
+
+
+
+    public static function getCounter() {
+        
+
+        return self :: $movieCounter;
     }
 
 }
 $releaseDate1 = new ReleaseDate("2020", "10", "05");
 $releaseDate2 = new ReleaseDate("2021", "01", "10");
 
-$movie1 = new Movie("Festazza", 2.00, $releaseDate1, "Drammatico", "Comico");
-$movie2 = new Movie("Il Dilemma", 1.30, $releaseDate2, "Comico");
-
-echo "<span>" . var_dump($movie1->title) . "</span>";
+$movie1 = new Movie("Festazza", 120, $releaseDate1, "Drammatico", "Comico");
+$movie2 = new Movie("Il Dilemma", 90, $releaseDate2, "Comico");
+echo $movie1 -> getMovieDetails();
 echo "<br> <br>";
-echo "<span>" . var_dump($movie1->genre) . "</span>";
-echo "<br> <br>";
-echo "<span>" . var_dump($movie1->duration) . "</span>";
-echo "<br> <br>";
-echo "<span>" . var_dump($movie1->releaseDate) . "</span>";
-echo "<br> <br>";
-echo "<br> <br>";
-echo "<span>" . var_dump($movie2->title) . "</span>";
-echo "<br> <br>";
-echo "<span>" . var_dump($movie2->genre) . "</span>";
-echo "<br> <br>";
-echo "<span>" . var_dump($movie2->duration) . "</span>";
-echo "<br> <br>";
-echo "<span>" . var_dump($movie2->releaseDate) . "</span>";
-echo "<br> <br>";
+echo $movie2 -> getMovieDetails();
